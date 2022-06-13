e6a706a194dc4a3dddc097222faf52e9b80d4ab8
Juan Del rosso <jdel-ros@z4r6p7.42lyon.fr>
2022-06-13 08:51:23 +0200

    home in progress

--- api/app.js
+++ api/app.js
@@ -12,19 +12,6 @@ const corsOptions = {
 var bodyParser = require('body-parser');
 var {initTables} =  require("./db");
 
-var multer = require('multer');
-var path = require('path')
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-  }
-})
-
-var upload = multer({ storage: storage });
 initTables();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
